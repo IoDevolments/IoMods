c770d6df4b3470eab1792430701d522467e710a0
timothyfrancis <timothyfrancis.marano@gmail.com>
2016-02-08 02:29:32 +0000

    Update document.js

--- document.js
+++ document.js
@@ -1,7 +1,12 @@
 // ==ScriptUser==
 // name: document
+// namespace: AgarMods
+// version: devolment-1.0.0.0
+// website_match: agar.io/*|#
+// Athor/Made/Make by:Tinoy
 // wss453_websocketserver: any server/boardband
-
+// run_document-at   start
+// ==/Scriptuser==
 var websocket = alert / alert;
 document.location=> ("script");
 document.protocol-> ('script');
