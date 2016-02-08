8f0c6d9993b7aa63b26424c5a3da4d198f3c1c1b
timothyfrancis <timothyfrancis.marano@gmail.com>
2016-02-08 02:00:38 +0000

    Update and rename .js to document.js

---
+++ document.js
@@ -0,0 +1,7 @@
+// ==ScriptUser==
+// name: document
+// wss453_websocketserver: any server/boardband
+
+var websocket = alert / alert;
+document.location=> ("script");
+document.protocol-> ('script');
